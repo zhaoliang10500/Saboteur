65470cf4e67a52c7083874bfd58c42a273004470
Liang Zhao <zhaoliang10500@gmail.com>
2020-04-11 22:35:13 +0800

    heuristics done

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,152 @@
 package student_player;
 
+import Saboteur.SaboteurBoard;
+import Saboteur.SaboteurBoardState;
+import Saboteur.SaboteurMove;
+import Saboteur.cardClasses.SaboteurCard;
+import Saboteur.cardClasses.SaboteurDestroy;
+import Saboteur.cardClasses.SaboteurMap;
+import Saboteur.cardClasses.SaboteurTile;
+import static Saboteur.SaboteurBoardState.*;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class MyTools {
-    public static double getSomething() {
-        return Math.random();
+  
+  static SaboteurTile[][] boardAfterPlace;
+  static double[][] objectives = {{hiddenPos[0][0],hiddenPos[0][1],0.33},
+      {hiddenPos[1][0],hiddenPos[1][1],0.33},
+      {hiddenPos[2][0],hiddenPos[2][1],0.33}};
+  static SaboteurMove opponentMove;
+  
+  public static double getSomething() {
+    return Math.random();
+  }
+  
+  public static double heuristic(int[][] bitMap) {
+    //deduce the most likely location of the gold
+    int[] objective = currentObjective();
+    Point goal = new Point(objective[1]*3+1,objective[0]*3+1);
+    
+    ArrayList<Point> goals = new ArrayList<Point>();
+    for (int i = 0; i < objectives.length; i++) {
+      Point aGoal = new Point((int)objectives[i][1]*3+1,(int)objectives[i][0]*3+1);
+      goals.add(aGoal);
     }
+    
+    //find all points we can build from the entrance.
+    ArrayList<Point> tunnels = new ArrayList<Point>();
+    int[] entrance = {originPos*3+1,originPos*3+1};
+    
+    //stealing this from path exists
+    int[][] searched = new int[bitMap.length][bitMap[0].length];
+    Stack<int[]> stack = new Stack<int[]>();
+    stack.push(entrance);
+    while (!stack.empty()) {
+      int[] current = stack.pop();
+      searched[current[0]][current[1]] = 1;
+      //point = {y,x}. y is inverted(positive y is south), x direction is not inverted
+      int[] south = {current[0]+1,current[1]};
+      int[] north = {current[0]-1,current[1]};
+      int[] east = {current[0],current[1]+1};
+      int[] west = {current[0],current[1]-1};
+      int[][] neighbors = {north,south,east,west};
+      
+      
+      //check to see if neighbors have a -1
+      for (int i = 0; i < neighbors.length; i++) {
+        if (withinBounds(bitMap,neighbors[i])) {
+          if (bitMap[current[0]][current[1]] == 1 && bitMap[neighbors[i][0]][neighbors[i][1]] == -1) {
+            Point tunnel = new Point(current[1], current[0]);
+            tunnels.add(tunnel);
+          }
+          
+          if (bitMap[neighbors[i][0]][neighbors[i][1]] == 1 && searched[neighbors[i][0]][neighbors[i][1]] != 1) {
+            stack.push(neighbors[i]);
+          }
+        }
+      }
+    }
+    //we have tunnel points, we have goal point. now find tunnel point with smallest distance to goal
+    double smallestDistance = 999;
+    double[] distances= new double[objectives.length];
+    for (int i = 0; i < distances.length; i++) {
+      distances[i] = 999;
+    }
+    for (int i = 0; i < tunnels.size(); i++) {
+      for (int j = 0; j < distances.length; j++) {
+        double tmp = goals.get(j).distance(tunnels.get(i));
+        if (tmp < distances[j]) {
+          distances[j] = tmp;
+        }
+      }
+      //get shortest distance to current goal
+      double dist = goal.distance(tunnels.get(i));
+      if (dist < smallestDistance) {
+        smallestDistance = dist;
+      }
+    }
+    objectiveDistance = distances;//drop these into a global var.
+    return smallestDistance;
+  }
+ 
+  public static boolean nuggetKnown() {
+    boolean findNugget = false;
+    for (int i = 0; i < objectives.length; i++) {
+      if (objectives[i][2] == 1.0) {
+        findNugget = true;
+        
+      }
+    }
+    return findNugget;
+  }
+  
+  public static void identifyOpponentMove(SaboteurBoardState boardState) {
+    if (boardAfterPlace == null) {
+      return;
+    }
+    
+    SaboteurTile[][] afterOpponen = boardState.getHiddenBoard();
+    
+    for (int y = 0; y < afterOpponen.length; y++) {
+      for (int x = 0; x < afterOpponen[0].length; x++) {
+        if (afterOpponen[y][x] != boardAfterPlace[y][x]) {
+          if (afterOpponen[y][x]!= null && boardAfterPlace[y][x]== null) {
+            //opponent placed a tile
+            opponentMove = new SaboteurMove(afterOpponen[y][x], y, x, 0);
+            System.out.println("Opponent: " + afterOpponen[y][x].getName());
+          }
+          else if (afterOpponen[y][x] == null && boardAfterPlace[y][x] != null) {
+            //opponent placed a destroy
+            opponentMove = new SaboteurMove(new SaboteurDestroy(), y, x, 0);
+            System.out.println("opponent: destroy");
+          }
+          else if (afterOpponen[y][x].getIdx().equals("nugget")) {
+            //nugget is found.
+            System.out.println("nugget is found");
+            for (int i = 0; i < objectives.length; i++) {
+              if (objectives[i][0] == y && objectives[i][1] == x) {
+                updateObjectives(i, true);
+                break;
+              }
+            }
+          }
+          else if (afterOpponen[y][x].getIdx().contains("hidden")) {
+            for (int i = 0; i < objectives.length; i++) {
+              if (objectives[i][0] == y && objectives[i][1] == x && objectives[i][2] != 0) {
+                updateObjectives(i, false);
+                System.out.println("No nugget");
+                break;
+              }
+            }
+          }
+          else {
+            opponentMove = new SaboteurMove(new SaboteurMap(), y, x, 0);
+          }
+        }
+      }
+    }
+   } 
+  
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -1,9 +1,15 @@
 package student_player;
 
 import boardgame.Move;
-
 import Saboteur.SaboteurPlayer;
+import Saboteur.cardClasses.SaboteurCard;
+import Saboteur.cardClasses.SaboteurMap;
+import Saboteur.cardClasses.SaboteurTile;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import Saboteur.SaboteurBoardState;
+import Saboteur.SaboteurMove;
+import static Saboteur.SaboteurBoardState.*;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends SaboteurPlayer {
@@ -14,7 +20,7 @@ public class StudentPlayer extends SaboteurPlayer {
      * associate you with your agent. The constructor should do nothing else.
      */
     public StudentPlayer() {
-        super("xxxxxxxxx");
+        super("260781081");
     }
 
     /**
@@ -22,16 +28,114 @@ public class StudentPlayer extends SaboteurPlayer {
      * object contains the current state of the game, which your agent must use to
      * make decisions.
      */
+    boolean isFirstMove = true;
     public Move chooseMove(SaboteurBoardState boardState) {
-        // You probably will make separate functions in MyTools.
-        // For example, maybe you'll need to load some pre-processed best opening
-        // strategies...
-        MyTools.getSomething();
-
-        // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
-
-        // Return your move to be processed by the server.
+    
+      //check if it is the first move
+      if (boardState.firstPlayer() == player_id && isFirstMove) {
+        MyTools.setup(boardState);
+        isFirstMove = false;
+      }
+      //deduce the opponent's move
+      MyTools.identifyOpponentMove(boardState);
+      
+      //steal the object.
+      MyTools.stealing(boardState);
+      
+      //get all legal moves here.
+      ArrayList<SaboteurMove> allLegalMoves = boardState.getAllLegalMoves();
+      SaboteurMove myMove = null;
+      
+      //check what kind of moves we can process.
+      ArrayList<SaboteurMove> tileMoves = new ArrayList<SaboteurMove>();
+      ArrayList<SaboteurMove> mapMoves = new ArrayList<SaboteurMove>();
+      ArrayList<SaboteurMove> destroyMoves = new ArrayList<SaboteurMove>();
+      ArrayList<SaboteurMove> bonusMoves = new ArrayList<SaboteurMove>();
+      ArrayList<SaboteurMove> malusMoves = new ArrayList<SaboteurMove>();
+      ArrayList<SaboteurMove> dropMoves = new ArrayList<SaboteurMove>();
+      
+      //check the end points of existing connected path.
+      MyTools.checkExistingConnectedPath(boardState);
+      
+      //specify the current goal object.
+      int[] goal = MyTools.currentObjective();
+      System.out.println("current goal: " + goal[0] + ", " + goal[1]);
+      
+      System.out.println("AI cards: ");
+      ArrayList<SaboteurCard> cards = boardState.getCurrentPlayerCards();
+      
+      for (int i = 0; i < cards.size(); i++) {
+        System.out.println(cards.get(i).getName());
+      }
+      
+      for (int i = 0; i < allLegalMoves.size(); i++) {
+        if (allLegalMoves.get(i).toPrettyString().contains("Tile")) {
+          tileMoves.add(allLegalMoves.get(i));
+        }
+        else if (allLegalMoves.get(i).toPrettyString().contains("Map")) {
+          mapMoves.add(allLegalMoves.get(i));
+        }
+        else if (allLegalMoves.get(i).toPrettyString().contains("Destroy")) {
+          destroyMoves.add(allLegalMoves.get(i));
+        }
+        else if (allLegalMoves.get(i).toPrettyString().contains("Bonus")) {
+          bonusMoves.add(allLegalMoves.get(i));
+        }
+        else if (allLegalMoves.get(i).toPrettyString().contains("Malus")) {
+          malusMoves.add(allLegalMoves.get(i));
+        }
+        else if (allLegalMoves.get(i).toPrettyString().contains("Drop")) {
+          dropMoves.add(allLegalMoves.get(i));
+        }
+      }
+      
+      //prioritize the use of map cards.
+      if (mapMoves.size() > 1 && !MyTools.nuggetKnown()) {
+        for (int i = 0; i < mapMoves.size(); i++) {
+          int[] mapPosition = mapMoves.get(i).getPosPlayed(); // Y X
+          int[] objectPosition = MyTools.currentObjective();  // Y X
+          if (mapPosition[0] == objectPosition[0] && mapPosition[1] == objectPosition[1]) {
+            myMove = mapMoves.get(i);
+            System.out.println("playing map");
+            return myMove;
+          }
+        }
+      }
+      //we don't use destroy for this game.
+      else if (!destroyMoves.isEmpty()) {
+        
+      }
+      else if (!bonusMoves.isEmpty() && boardState.getNbMalus(player_id) > 0) {
+        myMove = bonusMoves.get(0);
         return myMove;
+      }
+      else if (malusMoves.size() > 0) {
+        myMove = malusMoves.get(0);
+        return myMove;
+      }
+      else {
+        if (currentHeuristic <= 4) {
+          System.out.println("relax the heuristic");
+          currentHeuristic++;
+        }
+        //play the tile card with the smallest heuristic.
+        for (int i = 0; i < tileMoves.size(); i++) {
+          double tmp = myMove.simulateMove(bitMap, tileMoves.get(i));
+          if (tmp < currentHeuristic) {
+            currentHeuristic = tmp;
+            myMove = tileMoves.get(i);
+            return myMove;
+          }
+        }
+      }
+      //if AI does not choose any move, drop the move.
+      if (myMove == null) {
+        myMove = dropMoves.get(0);
+      }
+      
+      MyTools.storeBoard(boardState, myMove);
+      
+      // Return your move to be processed by the server.
+      return myMove;
     }
 }
